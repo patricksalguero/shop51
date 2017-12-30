8c7b86b569e4c271492a0b0700fb673f6aefa49e
Patrick Salguero <patrick.salguero.avalos@gmail.com>
2017-12-30 00:24:33 -0500

    Se modifica el componentes Clientes para mostrar Listado de Clientes

--- front/src/app/components/clients/clients.component.ts
+++ front/src/app/components/clients/clients.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientsComponent implements OnInit {
 
+  title : string = "Clientes";
+
   constructor() { }
 
   ngOnInit() {
--- front/src/app/components/providers/providers.component.ts
+++ front/src/app/components/providers/providers.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProvidersComponent implements OnInit {
 
+  title : string = "Proveedores";
+
   constructor() { }
 
   ngOnInit() {
